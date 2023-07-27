1a42bf4a44810257d054ceaf5b462e4f9c46e3c5
linlin <1208215066@qq.com>
2023-07-27 15:28:41 +0800

    在 src/App.js 中创建两个页面，使用 React Router 来将它们组合在一起, 在src/reducers/counter.js 文件中定义 reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './store';
+import Counter from './Counter';
+import Coutrol from './Control';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Counter></Link>
+              </li>
+              <li>
+                <Link to='/control'>Counter></Link>
+              </li>
+            </ul>
+          </nav>
+          <Route exact path="/" component={Counter} />
+          <Route exact path="/control" component={Control} />
+        </div>
+      </Router>
+    </Provider>
+  )
 }
 
 export default App;
+
+//import React from 'react';
+//import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+//import { Provider } from 'react-redux';
+//import store from './store';
+//import Counter from './Counter';
+//import Control from './Control';
+
+//function App() {
+//  return (
+//    <Provider store={store}>
+//      <Router>
+//        <div>
+//          <nav>
+//            <ul>
+//              <li>
+//                <Link to="/">Counter</Link>
+//              </li>
+//              <li>
+//                <Link to="/control">Control</Link>
+//              </li>
+//            </ul>
+//          </nav>
+
+//          <Route exact path="/" component={Counter} />
+//          <Route path="/control" component={Control} />
+//        </div>
+//      </Router>
+//    </Provider>
+//  );
+//}
+
+//export default App;
